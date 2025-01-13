185888867882e422e40acc02a036f5c2e51a7826
Marlon Santos <xsantos.marlonx@gmail.com>
2025-01-13 17:39:53 -0300

    Done the formation of slide in page

--- src/slide.js
+++ src/slide.js
@@ -1,17 +1,13 @@
-var _a, _b;
+var _a;
 var currentSlider = 0;
 var totalSlides = document.querySelectorAll("#slider-tems").length;
 var sliderBtn = document.querySelector("#slider-controls");
-var sliderBtnC = document.querySelector("#slider-control");
 var sliderWidth = document.querySelector("#slider-width");
 if (sliderWidth) {
     sliderWidth.style.width = "calc(100vw * ".concat(totalSlides, ")");
 }
 if (sliderBtn) {
     sliderBtn.style.height = "".concat((_a = document.querySelector("#slider")) === null || _a === void 0 ? void 0 : _a.clientHeight, "px");
-}
-if (sliderBtnC) {
-    sliderBtnC.style.height = "".concat((_b = document.querySelector("#slider")) === null || _b === void 0 ? void 0 : _b.clientHeight, "px");
 }
 function goBack() {
     currentSlider--;
