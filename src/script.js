235768e6ea11790580b0d2ddcad881d836161d37
Marlon Santos <xsantos.marlonx@gmail.com>
2025-02-15 11:47:54 -0300

    refactoring all the TS code

--- src/script.js
+++ src/script.js
@@ -1,95 +1,43 @@
-var items = document.querySelectorAll(".items");
 var closedModalBtn = document.querySelector("#close-modal");
 var shoppingCart = document.getElementById("shopping-cart");
 var buttonPay = document.querySelector("#payment");
 var fade = document.querySelector("#fade");
 var modal = document.querySelector("#modal");
 var car = document.querySelector(".car");
-var quntityCart = document.querySelectorAll(".quantity-cart-product");
-for (var i = 0; i < (quntityCart === null || quntityCart === void 0 ? void 0 : quntityCart.length); i++) {
-    quntityCart[i].addEventListener("change", updateInfoCart);
+//--------------
+//remover o produto do carrinho
+var removeProductCartBtn = document.querySelectorAll(".remove-product-cart");
+for (var i = 0; i < removeProductCartBtn.length; i++) {
+    removeProductCartBtn[i].addEventListener("click", function (element) {
+        var target = element.target;
+        var elementProduct = target.closest(".product-in-cart");
+        if (elementProduct) {
+            elementProduct.remove();
+        }
+        updateTotal();
+    });
 }
 //--------------
-function updateInfoCart() {
-    //Atualiza o valor no carrinho de comprar ao remover o produto
-    var productInCart = document.querySelectorAll(".product-in-cart");
-    var totalProduct = 0;
-    for (var i = 0; i < productInCart.length; i++) {
-        var priceProductCart = productInCart[i].querySelectorAll(".price-product-cart")[0].innerText.replace("R$", "").replace(",", ".");
-        var quantityProductCart = productInCart[i].querySelectorAll(".quantity-cart-product")[0].value;
-        totalProduct += (+priceProductCart * +quantityProductCart);
+//Atualizando o valor dos produtos dentro do carrinho
+function updateTotal() {
+    var totalValue = 0;
+    var updateValueCart = document.querySelectorAll(".product-in-cart");
+    for (var i = 0; i < updateValueCart.length; i++) {
+        var priceProductCart = updateValueCart[i].querySelectorAll(".price-product-cart")[0].innerText.replace("R$", "").replace(",", ".");
+        var inputQuantityCart = updateValueCart[i].querySelectorAll(".quantity-cart-product")[0].value;
+        totalValue += (+priceProductCart * +inputQuantityCart);
     }
-    totalProduct = totalProduct.toFixed(2);
-    totalProduct = totalProduct.replace(".", ",");
-    var totalPrice = document.querySelector(".total-price span");
-    totalPrice.innerText = "R$ ".concat(totalProduct.toString());
+    totalValue = totalValue.toFixed(2).replace(".", ",");
+    var updateSpanValue = document.querySelector(".total-price span");
+    if (updateSpanValue) {
+        updateSpanValue.innerText = "R$ ".concat(totalValue.toString());
+    }
 }
 //--------------
-var removeProductBtn = document.querySelectorAll(".remove-product");
-for (var i = 0; i < removeProductBtn.length; i++) {
-    // Evento para remover os produtos do carrinho de compras
-    removeProductBtn[i].addEventListener("click", function (event) {
-        var targetElement = event.target;
-        var targetProduct = targetElement.closest("tr");
-        if (targetProduct) {
-            targetProduct.remove();
-        }
-        updateInfoCart();
-    });
-}
 //--------------
-var addCart = document.querySelectorAll(".add-cart");
-addCart.forEach(function (event) {
-    //Essa função adiciona os itens no carrinho
-    event.addEventListener("click", function addItemCart(element) {
-        var _a, _b, _c;
-        var elementTarget = element.target;
-        var products = elementTarget.closest(".all-products-move");
-        if (!products)
-            return;
-        var imageProduct = ((_a = products.querySelector(".image-product")) === null || _a === void 0 ? void 0 : _a.src) || "";
-        var titleProduct = ((_b = products.querySelector(".title-product")) === null || _b === void 0 ? void 0 : _b.innerText) || "Produto sem título";
-        var price = ((_c = products.querySelector(".price")) === null || _c === void 0 ? void 0 : _c.innerText) || "0,00";
-        var createNewProduct = document.createElement("tr");
-        createNewProduct.classList.add("product-in-cart");
-        createNewProduct.innerHTML =
-            "\n         <td class=\"pt-4 border-b-orange-800\">\n         <div class=\"w-24 max-w-full max-h-full p-1 bg-orange-100 rounded-md\">\n              <h2 class=\"uppercase tracking-wider text-orange-800 text-center\">".concat(titleProduct, "</h2>\n              <img class=\"rounded-md\" src=\"").concat(imageProduct, "\" alt=\"").concat(titleProduct, "\">\n         </div>\n      </td>\n\n      <td>\n        <span class=\"price-product-cart text-gray-700 font-bold\">R$ ").concat(price, "</span>\n      </td>\n\n      <td>\n        <input class=\"quantity-cart-product w-16 outline-none border-none rounded-md text-center text-gray-700 font-bold p-1\" type=\"number\" value=\"1\">\n      </td>\n\n      <td>\n        <button class=\"remove-product w-5 h-5 flex justify-center items-center p-3 bg-orange-200 border border-orange-700 rounded-full text-white\">X</button>\n      </td>\n        ");
-        var bodyTable = document.querySelector(".add-tbody");
-        if (bodyTable) {
-            bodyTable === null || bodyTable === void 0 ? void 0 : bodyTable.appendChild(createNewProduct);
-        }
-        updateInfoCart();
-    });
-});
-items.forEach(function (product) {
-    var _a;
-    var btnLess = product.querySelector(".btn-less");
-    var btnMore = product.querySelector(".btn-more");
-    var quatity = product.querySelector(".quantity");
-    var price = product.querySelector(".price");
-    if (!btnLess || !btnMore || !quatity || !price)
-        return;
-    var count = parseInt(quatity.textContent || "1", 10);
-    var convertPrice = parseFloat(((_a = price.textContent) === null || _a === void 0 ? void 0 : _a.replace("R$", "").trim()) || "0");
-    var updatePrice = function () {
-        var priceNew = (convertPrice * count).toFixed(2);
-        price.textContent = "R$ ".concat(priceNew);
-    };
-    btnLess.addEventListener("click", function () {
-        if (count > 1) {
-            count--;
-            quatity.textContent = count.toString();
-            updatePrice();
-        }
-    });
-    btnMore.addEventListener("click", function () {
-        count++;
-        quatity.textContent = count.toString();
-        updatePrice();
-    });
-    buttonPay === null || buttonPay === void 0 ? void 0 : buttonPay.addEventListener("click", function () {
-        toggleModal();
-    });
+//--------------    
+buttonPay === null || buttonPay === void 0 ? void 0 : buttonPay.addEventListener("click", function () {
+    toggleModal();
 });
 car === null || car === void 0 ? void 0 : car.addEventListener("click", function () {
     if (shoppingCart) {
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,3 @@
-const items = document.querySelectorAll<HTMLElement>(".items");
-
 const closedModalBtn = document.querySelector<HTMLButtonElement>("#close-modal");
 const shoppingCart = document.getElementById("shopping-cart") as unknown as HTMLElement | null;
 const buttonPay = document.querySelector<HTMLButtonElement>("#payment");
@@ -7,140 +5,57 @@ const fade = document.querySelector<HTMLElement>("#fade");
 const modal = document.querySelector<HTMLElement>("#modal");
 const car = document.querySelector<HTMLElement>(".car");
 
+//--------------
 
-const quntityCart = document.querySelectorAll<HTMLInputElement>(".quantity-cart-product");
-for(let i = 0; i < quntityCart?.length; i++){
-    quntityCart[i].addEventListener("change", updateInfoCart);
+//remover o produto do carrinho
+const removeProductCartBtn = document.querySelectorAll<HTMLButtonElement>(".remove-product-cart");
+for(let i = 0; i < removeProductCartBtn.length; i++){
+  removeProductCartBtn[i].addEventListener("click", function(element) {
+    const target = element.target as HTMLElement;
+
+    const elementProduct = target.closest(".product-in-cart");
+
+    if(elementProduct){
+      elementProduct.remove();
+    }
+    updateTotal();
+  });
+}
+
+//--------------
+//Atualizando o valor dos produtos dentro do carrinho
+function updateTotal(){
+
+    let totalValue: string | number = 0;
+    
+    const updateValueCart = document.querySelectorAll<HTMLElement>(".product-in-cart");
+    for (let i = 0; i< updateValueCart.length; i++){
+      const priceProductCart = updateValueCart[i].querySelectorAll<HTMLSpanElement>(".price-product-cart")[0].innerText.replace("R$", "").replace(",", ".");
+      const inputQuantityCart = updateValueCart[i].querySelectorAll<HTMLInputElement>(".quantity-cart-product")[0].value;
+      
+      totalValue +=  (+priceProductCart * +inputQuantityCart);
+    }
+
+    totalValue = totalValue.toFixed(2).replace(".", ",");
+    
+    const updateSpanValue = document.querySelector<HTMLSpanElement>(".total-price span");
+    if(updateSpanValue){
+      updateSpanValue.innerText = `R$ ${totalValue.toString()}`;
+    }
 }
 
 //--------------
 
-function updateInfoCart():void{
-  //Atualiza o valor no carrinho de comprar ao remover o produto
-  const productInCart = document.querySelectorAll<HTMLElement>(".product-in-cart");
-  let totalProduct: string | number = 0;
-  
-  for(let i = 0; i < productInCart.length; i++){
-    const priceProductCart = (productInCart[i].querySelectorAll(".price-product-cart")[0] as HTMLSpanElement).innerText.replace("R$", "").replace(",", ".");
-    const quantityProductCart = (productInCart[i].querySelectorAll(".quantity-cart-product")[0] as HTMLInputElement).value;
-    
-    totalProduct +=  (+priceProductCart * +quantityProductCart);
-  }
-  totalProduct = totalProduct.toFixed(2);
-  totalProduct = totalProduct.replace(".", ",");
-  
-  const totalPrice = (document.querySelector(".total-price span") as HTMLSpanElement);
-  totalPrice.innerText = `R$ ${totalProduct.toString()}`;
-}
+
 
 //--------------
 
-const removeProductBtn = document.querySelectorAll<HTMLButtonElement>(".remove-product");
-for (let i = 0; i < removeProductBtn.length; i++){
 
-  // Evento para remover os produtos do carrinho de compras
+//--------------    
 
-  removeProductBtn[i].addEventListener("click", function (event): void {
-       const targetElement = event.target as HTMLElement;
-       const targetProduct = targetElement.closest("tr");
-
-       if(targetProduct){
-        targetProduct.remove();
-       }
-       updateInfoCart();
-    });
-}
-
-//--------------
-
-const addCart = document.querySelectorAll<HTMLButtonElement>(".add-cart");
-    
-    addCart.forEach((event)=>{
-      //Essa função adiciona os itens no carrinho
-      event.addEventListener("click", function addItemCart(element): void{
-        const elementTarget = element.target as HTMLElement;
-
-        const products = elementTarget.closest(".all-products-move");
-        if(!products) return;
-        
-        const imageProduct = products.querySelector<HTMLImageElement>(".image-product")?.src || "";
-        const titleProduct = products.querySelector<HTMLElement>(".title-product")?.innerText || "Produto sem título";
-        const price = products.querySelector<HTMLSpanElement>(".price")?.innerText || "0,00";
-        
-        let createNewProduct = document.createElement("tr");
-        createNewProduct.classList.add("product-in-cart");
-
-        createNewProduct.innerHTML = 
-        `
-         <td class="pt-4 border-b-orange-800">
-         <div class="w-24 max-w-full max-h-full p-1 bg-orange-100 rounded-md">
-              <h2 class="uppercase tracking-wider text-orange-800 text-center">${titleProduct}</h2>
-              <img class="rounded-md" src="${imageProduct}" alt="${titleProduct}">
-         </div>
-      </td>
-
-      <td>
-        <span class="price-product-cart text-gray-700 font-bold">R$ ${price}</span>
-      </td>
-
-      <td>
-        <input class="quantity-cart-product w-16 outline-none border-none rounded-md text-center text-gray-700 font-bold p-1" type="number" value="1">
-      </td>
-
-      <td>
-        <button class="remove-product w-5 h-5 flex justify-center items-center p-3 bg-orange-200 border border-orange-700 rounded-full text-white">X</button>
-      </td>
-        `;
-
-        const bodyTable = document.querySelector<HTMLElement>(".add-tbody");
-        if(bodyTable){
-          bodyTable?.appendChild(createNewProduct);
-        }
-
-        updateInfoCart();
-      });
-
-    });
-
-items.forEach((product) => {
-    const btnLess = product.querySelector<HTMLButtonElement>(".btn-less");
-    const btnMore = product.querySelector<HTMLButtonElement>(".btn-more");
-    const quatity = product.querySelector<HTMLSpanElement>(".quantity");
-    const price = product.querySelector<HTMLSpanElement>(".price");
-    
-    
-
-    if (!btnLess || !btnMore || !quatity || !price) return;
-
-    let count: number = parseInt(quatity.textContent || "1", 10);
-    let convertPrice: number = parseFloat(
-      price.textContent?.replace("R$", "").trim() || "0"
-    );
-
-    const updatePrice = () => {
-      const priceNew = (convertPrice * count).toFixed(2);
-      price.textContent = `R$ ${priceNew}`;
-    };
-
-    btnLess.addEventListener("click", () => {
-      if (count > 1) {
-        count--;
-        quatity.textContent = count.toString();
-        updatePrice();
-      }
-    });
-
-    btnMore.addEventListener("click", () => {
-      count++;
-      quatity.textContent = count.toString();
-      updatePrice();
-    });
-
-    buttonPay?.addEventListener("click", ()=>{
-      toggleModal();
-    });   
-
-});
+buttonPay?.addEventListener("click", ()=>{
+  toggleModal();
+}); 
 
 car?.addEventListener("click", () => {
   if (shoppingCart) {
